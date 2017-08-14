f079f23798ffa419d17b09299018ebf98cce126a
Gijs van Zon <gijs.van.zon@freshheads.com>
2017-08-14 13:58:03 +0200

    Updated exporting.js to be compliant with example

--- Exporter.sketchplugin/Contents/Sketch/exporter.js
+++ Exporter.sketchplugin/Contents/Sketch/exporter.js
@@ -1,6 +1,6 @@
-// Exporter, by Sam Deane — Source code available at [GitHub](https://github.com/BohemianCoding/plugins.examples.exporter)
+// Exporting Examples, by Sam Deane — Source code available at [GitHub](https://github.com/BohemianCoding/plugins.examples.exporter)
 //
-// This is an example plugin, which illustrates how to export layers.
+// This is an example plugin, which illustrates how to export layers, artboards and pages.
 
 //
 // ## Layout
@@ -26,19 +26,33 @@
 //
 //  ```json
 // {
-//     "name" : "Hello World!",
-//     "identifier" : "com.sketchapp.examples.helloworld",
+//     "name" : "Exporting Examples",
+//     "identifier" : "com.sketchapp.examples.exporter",
 //     "version" : "1.0",
-//     "description" : "Pretty much the smallest example Sketch Plugin you could have.",
+//     "description" : "Examples showing how to export things from a plugin.",
 //     "authorEmail" : "sam@sketchapp.com",
 //     "author" : "Sam Deane",
 //     "commands" : [
 //     {
-//       "script" : "hello-world.js",
-//       "handler" : "onRun",
+//       "script" : "exporting.js",
+//       "handler" : "onExportLayers",
 //       "shortcut" : "",
-//       "name" : "Hello World!",
-//       "identifier" : "helloworld"
+//       "name" : "Export Selected Layers",
+//       "identifier" : "exportLayers"
+//     },
+//     {
+//       "script" : "exporting.js",
+//       "handler" : "onExportPage",
+//       "shortcut" : "",
+//       "name" : "Export Page",
+//       "identifier" : "exportPage"
+//     },
+//     {
+//       "script" : "exporting.js",
+//       "handler" : "onExportArtboards",
+//       "shortcut" : "",
+//       "name" : "Export Artboards",
+//       "identifier" : "exportArtboards"
 //     }
 //   ]
 // }
@@ -47,22 +61,63 @@
 // ## Code
 // ### Defining The Run Handler
 //
-// In the manifest, we told Sketch that every time the "Hello World!" menu is selected,
-// we want to execute  a javascript handler called `onRun`.
+// In the manifest, we told Sketch that every time a "Exporting Examples / exporting function" menu item is selected,
+// we want to execute a javascript handler called `onExportLayers`,`onExportPage` or `onExportArtboards` based on the selected menu item.
 //
-// So now we need to put some code into the `hello-world.js` file to implement that command.
+// So now we need to put some code into the `exporting.js` file to implement those commands.
 
 function onExportLayers(context) {
 
-  // We are passed a context variable when we're run.
-  // We use this to get hold of a javascript object
-  // which we can use in turn to manipulate Sketch.
-  var sketch = context.api()
+    // Fetch the root Sketch object
+    var sketch = context.api()
 
-  // Next we want to extract the selected page of the selected (front-most) document
-  var document = sketch.selectedDocument
-  var page = document.selectedPage
+    // We can specify a lot of different options for the exporting.
+    //
+    // ### General Options
+    //
+    // - use-id-for-name : normally the exported files are given the same names as the layers they represent, but if this options is true, then the layer ids are used instead; defaults to false.
+    // - output : this is the path of the folder where all exported files are placed; defaults to "~/Documents/Sketch Exports"
+    // - overwriting : if true, the exporter will overwrite any existing files with new ones; defaults to false.
+    // - trimmed: if true, any transparent space around the exported image will be trimmed; defaults to false.
+    // - scales: this should be a list of numbers; it will determine the sizes at which the layers are exported; defaults to "1"
+    // - formats: this should be a list of one or more of "png", "jpg", "svg", and "pdf"; defaults to "png" (see discussion below)
+    //
+    // ### SVG options
+    //
+    // - compact : if exporting as SVG, this option makes the output more compact; defaults to false.
+    // - include-namespaces : if exporting as SVG, this option includes extra attributes; defaults to false.
+    //
+    // ### PNG options
+    // - save-for-web : if exporting a PNG, this option removes metadata such as the colour profile from the exported file; defaults to false.
+    //
+    // ### JPG options
+    // - compression : if exporting a JPG, this option determines the level of compression, with 0 being the minimum, 1.0 the maximum; defaults to 1.0
+    // - progressive : if exporting a JPG, this option makes it progressive; defaults to false.
+    // - group-contents-only : false,
 
+    var options = { "scales" : "1, 2, 3", "formats" : "png, jpg" }
+
+
+    // Iterate over each layer in the selection, exporting it
+    sketch.selectedDocument.selectedLayers.iterate(function(layer) {
+      layer.export(options)
+    })
+};
+
+function onExportPage(context) {
+
+    // Fetch the root Sketch object
+    var sketch = context.api()
+    var options = { "scales" : "1, 2, 3", "formats" : "png, jpg" }
+    sketch.selectedDocument.selectedPage.export(options)
+};
+
+function onExportArtboards(context) {
+
+    // Fetch the root Sketch object
+    var sketch = context.api()
+    var options = { "scales" : "1, 2, 3", "formats" : "png, jpg" }
+    sketch.selectedDocument.selectedPage.exportArtboards(options)
 };
 
 // And that's it. Job done.
